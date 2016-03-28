9a9283c0363f999eb168a3c8ba63c2f5629d517e
Omar Jbara <omar.jbara2@gmail.com>
2016-03-29 01:46:34 +0200

    added files

---
+++ src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\AppBundle;
+use AppBundle\Entity\Comment;
+use AppBundle\Entity\Page;
+use AppBundle\Entity\Post;
+use AppBundle\Entity\Role;
+use AppBundle\Entity\User;
+use DateTime;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Form\UserType;
+
+/**
+ * @Route("/admin", name="admin_index")
+ * @Security("has_role('ROLE_ADMIN')")
+ */
+class AdminController extends Controller
+{
+
+    /**
+     * @Route("/", name="admin_index")
+     */
+    public function indexAction(Request $request){
+        return $this->render('admin/index.html.twig');
+
+    }
+
+
+    /**
+     * @Route("/post/", name="blog_list_posts")
+     */
+    public function listPostAction(Request $request){
+        $em = $this->getDoctrine()->getRepository(Post::class);
+        $posts = $em->findAll('DESC');
+        return $this->render('admin/blog_list_posts.html.twig', ['posts' => $posts]);
+
+
+    }
+
+
+
+}
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,52 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Page;
+use AppBundle\Entity\Role;
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
+
+
     /**
      * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        $em = $this->getDoctrine()->getRepository(Page::class);
+        $pages = $em->findAll();
+        $token = $this->get('security.token_storage')->getToken()->setAuthenticated(false);
+        return $this->render('default/index.html.twig', ['title' => 'Accueil', 'menu' => $pages]);
     }
+
+
+    /**
+     * @Route(name="menu")
+     */
+    public function dislayMenuAction($classNames = ''){
+        $em = $this->getDoctrine()->getRepository(Page::class);
+        $pages = $em->findAll();
+        return $this->render('menu.html.twig', ['pages' => $pages, 'classNames' => $classNames]);
+    }
+    /**
+     * @Route("/{slug}", name="page")
+     */
+    public function showPageAction(Request $request, $slug){
+        $em = $this->getDoctrine()->getRepository(Page::class);
+        $page = $em->findOneBy(['slug' => $slug]);
+        if($page == null){
+            throw new NotFoundHttpException('Page non trouvé');
+        }
+        return $this->render('default/page.html.twig', ['page' => $page]);
+    }
+
+
+
+
+
 }
